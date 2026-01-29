c409bee362762017bf2acf3b0e0b911a754af002
karima-nr <karimanr12@gmail.com>
2026-01-29 23:51:36 +0700

    railway start

--- server.js
+++ server.js
@@ -344,3 +344,17 @@ process.on('SIGINT', () => {
   prisma.$disconnect();
   process.exit(0);
 });
+
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Backend Railway jalan ✅");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
